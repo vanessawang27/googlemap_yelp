73f78774c396a0844111972c8eceba35ada94925
Crongz <leejamesws@gmail.com>
2015-11-21 16:50:07 -0600

    api_key

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/static'));
 app.get('/', function(req, res){
  res.sendFile(__dirname + '/index.html');
 });
