c236c7bb8d5be2a5e47b6202f2a5789ba025e05b
Crongz <leejamesws@gmail.com>
2015-11-21 18:24:17 -0600

    error console

---
+++ static/js/yelp.js
@@ -0,0 +1,33 @@
+// Request API access: http://www.yelp.com/developers/getting_started/api_access
+var Yelp = require('yelp');
+
+var yelp = new Yelp({
+  consumer_key: 'GBltAhcIGaWBiohWyXukyQ',
+  consumer_secret: 'EZODy_uF2UCkd4uqCyOgrmNdmAY',
+  token: 'nHaXfHn0xY8WWQpC8B26egU08xdBKUQW',
+  token_secret: 'dP8MHaybnx0YMBjpUBg8EkaSpgE',
+});
+
+// See http://www.yelp.com/developers/documentation/v2/search_api
+yelp.search({ term: 'food', location: 'Montreal' })
+.then(function (data) {
+  console.log(data);
+})
+.catch(function (err) {
+  console.error(err);
+});
+
+// See http://www.yelp.com/developers/documentation/v2/business
+yelp.business('yelp-san-francisco')
+  .then(console.log)
+  .catch(console.error);
+
+yelp.phoneSearch({ phone: '+15555555555' })
+  .then(console.log)
+  .catch(console.error);
+
+// A callback based API is also available:
+yelp.business('yelp-san-francisco', function(err, data) {
+  if (err) return console.log(error);
+  console.log(data);
+});
